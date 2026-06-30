42433bf7ebe3ec57d7054d9e8b1c9a9a221812e8
Henrique AC <henriqueac00@gmail.com>
2026-06-30 14:12:21 -0300

    Inicio Projeto Spring Boot
    Setup do Spring Boot: pom.xml, application.properties

---
+++ src/main/java/org/gestaomvc/gestaojogosmvc/GestaojogosmvcApplication.java
@@ -0,0 +1,13 @@
+package org.gestaomvc.gestaojogosmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaojogosmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaojogosmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gestaomvc/gestaojogosmvc/GestaojogosmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gestaomvc.gestaojogosmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaojogosmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
